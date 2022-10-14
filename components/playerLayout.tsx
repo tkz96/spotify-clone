f454703be0ab14b7609ce71b2a1ab36ca7100a36
Talha <tkz9696@gmail.com>
2022-10-14 19:26:07 +0500

    Add dependencies; Test PlayerLayout component

---
+++ components/playerLayout.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/layout";
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box>
+      Testing PlayerLayout
+      {children}
+    </Box>
+  );
+};
+
+export default PlayerLayout;
